bd9f51890e1855d04ccc84541be819dbb000b81b
RafaelRFAndrade <110788109+RafaelRFAndrade@users.noreply.github.com>
2023-09-09 22:03:17 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/DevEvent.cs
@@ -0,0 +1,33 @@
+﻿namespace RestApi.Entities
+{
+    public class DevEvent
+    {
+
+        public DevEvent()
+        {
+            Speakers = new List<DevEventSpeakers>();
+            isDeleted = false;
+        }
+
+        public Guid id { get; set; }
+        public string Tittle { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<DevEventSpeakers> Speakers { get; set;}
+
+        public bool isDeleted { get; set; }
+
+        public void Update(string tittle, string description, DateTime startDate, DateTime endDate)
+        {
+            Tittle = tittle;
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+        public void Delete()
+        {
+            isDeleted = true;
+        }
+    }
+}
---
+++ Persistence/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using RestApi.Entities;
+
+namespace RestApi.Persistence
+{
+    public class DbContext
+    {
+        public List<DevEvent> DevEvents { get; set; }
+
+        public DbContext() 
+        {
+         DevEvents = new List<DevEvent>();
+        }
+    }
+}
